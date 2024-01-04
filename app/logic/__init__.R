fbbf92ee155345b8aeb8fa82d1cfe8c58870b9b2
AndrewDisher <ajdisher1998@gmail.com>
2024-01-04 12:50:57 -0500

    updated logic and view directory

--- app/logic/__init__.R
+++ app/logic/__init__.R
@@ -1,2 +1,10 @@
 # Logic: application code independent from Shiny.
 # https://go.appsilon.com/rhino-project-structure
+
+box::use(
+  ./constants,
+  
+  ./mbta_map_logic,
+  ./route_bar_chart_logic,
+  ./stop_bar_chart_logic
+)
--- app/view/__init__.R
+++ app/view/__init__.R
@@ -1,2 +1,8 @@
 # View: Shiny modules and related code.
 # https://go.appsilon.com/rhino-project-structure
+
+box::use(
+  ./mbta_map,
+  ./route_bar_chart,
+  ./stop_bar_chart
+)
