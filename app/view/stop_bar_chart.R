704f60e69d3da84ba03d1aeaabf7584cba56b21c
AndrewDisher <ajdisher1998@gmail.com>
2024-01-04 12:55:57 -0500

    Created ui for ma and bar charts, using echarts4r for the bar charts and leaflet for the map

---
+++ app/view/stop_bar_chart.R
@@ -0,0 +1,55 @@
+# -------------------------------------------------------------------------
+# ---------------------------- Libraries/Packages -------------------------
+# -------------------------------------------------------------------------
+
+box::use(
+  echarts4r[echarts4rOutput, renderEcharts4r],
+  shiny[moduleServer, NS, reactive, tagList, tags]
+)
+
+# -------------------------------------------------------------------------
+# ---------------------------------- Modules ------------------------------
+# -------------------------------------------------------------------------
+
+box::use(
+  app/logic[stop_bar_chart_logic]
+)
+
+# -------------------------------------------------------------------------
+# ------------------------------ UI Function ------------------------------
+# -------------------------------------------------------------------------
+
+init_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    tags$div(
+      class = "chart-bar-stop-container",
+      echarts4rOutput(outputId = ns("stop_bar_chart"), height = "300px")
+    )
+  )
+}
+
+# -------------------------------------------------------------------------
+# ----------------------------- Server Function ---------------------------
+# -------------------------------------------------------------------------
+
+init_server <- function(id, stop_data) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      # ----------------------------------
+      # ----- Reactive Data Cleaning -----
+      # ----------------------------------
+      cleaned_stop_data <- reactive({
+        stop_bar_chart_logic$clean_data(data = stop_data(), year = "2019")
+      })
+      
+      # -------------------------------
+      # ----- Build The Bar Chart -----
+      # -------------------------------
+      output$stop_bar_chart <- renderEcharts4r({
+        stop_bar_chart_logic$build_bar_chart(data = cleaned_stop_data())
+      })
+    }
+  )
+}
